e2ff7f6f16e07caf8c30156c96ae59d943db73cd
ryanmle2001 <ryanmle2001@gmail.com>
2022-02-19 02:02:05 -0500

    Google Auth config

--- rant-app/app.py
+++ rant-app/app.py
@@ -1,10 +1,20 @@
-import os
+from flask import Flask, url_for, render_template, request, redirect, abort, jsonify, session
+from google_auth_oauthlib.flow import Flow, google
 import pathlib
-
-from flask import Flask, render_template, request, redirect, abort, jsonify, session
-from google_auth_oauthlib.flow import Flow
-
+import os
+import requests
 app = Flask(__name__)
+app.secret_key="Hello"
+
+GOOGLE_CLIENT_ID = "829965262603-houdc4q9b3uohkn7t6v97mtic4ehg7og.apps.googleusercontent.com"
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(client_secrets_file=client_secrets_file, scopes=[
+    "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback")
 
 @app.route("/")
 def login():
@@ -50,12 +60,6 @@ def edit_my_user(username):
 def display_my_user(username):
     return f"Diplaying my account"
 
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("page_not_found.html"), 404
-
-
 @app.route('/api')
 def session_api():
     return jsonify(list(session.keys()))
@@ -68,15 +72,24 @@ def callback():
         abort(500)
 
     credentials = flow.credentials
-    request_session = request.sessions()
+    request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
     token_request = google.auth.transport.requests.Request(session=cached_session)
 
-    id_info = id_token.verify_oath2_token(
+    id_info = google.oauth2.id_token.verify_oath2_token(
         id_token=credentials._id_token,
         request=token_request,
         audience=GOOGLE_CLIENT_ID
     )
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page_not_found.html"), 404
+
+@app.errorhandler(500) #TODO add proper handler here
+def page_not_found(error):
+    return render_template("page_not_found.html"), 500
+
 
 
 if __name__ == "__main__":
