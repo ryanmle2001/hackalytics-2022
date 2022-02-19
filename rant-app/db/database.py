606e06b63b0e906d791a83f7c07e3252e4714d7a
ryanmle2001 <ryanmle2001@gmail.com>
2022-02-18 23:18:35 -0500

    Basic routing and database

---
+++ rant-app/db/database.py
@@ -0,0 +1,44 @@
+from pymongo import MongoClient
+from pprint import pprint
+import json
+
+
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient("localhost:27017")
+
+#db=client.admin
+# connect to database
+db = client.rant_app
+
+'''
+User fields
+- username (primary key): String
+- password ? 
+- First name: String
+- Last name: String
+- Display name: String
+- Interest: String[] 
+- Rants: rant_id[] 
+'''
+
+def get_user(username):
+    user = db.users.find_one({"username": username})
+    return user
+
+# String username
+def insert_user(user):
+    db.users.insert_one(user)
+
+#String username, dict field
+def update_user(username, field):
+    user = db.users.find_one({"username": username})
+    key = field.key()
+    value = field.value()
+    user[key] = value
+
+def update_user_post(username,
+def delete_user():
+    return False
+# Issue the serverStatus command and print the results
+# serverStatusResult=db.command("serverStatus")
+# pprint(serverStatusResult)
